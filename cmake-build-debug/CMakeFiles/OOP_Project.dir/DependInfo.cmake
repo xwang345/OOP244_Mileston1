03599d988ef009af253f3ec8af527ab2add871ae
ryan.wang23 <ryan.wang23@hotmail.com>
2017-07-07 14:42:59 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/zhi/OOP-Project/MS1/01-DefValTester.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/01-DefValTester.cpp.obj"
+  "C:/Users/zhi/OOP-Project/MS1/02-ConstructorTester.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/02-ConstructorTester.cpp.obj"
+  "C:/Users/zhi/OOP-Project/MS1/03-LogicalOperator.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/03-LogicalOperator.cpp.obj"
+  "C:/Users/zhi/OOP-Project/MS1/04-ErrorHandling.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/04-ErrorHandling.cpp.obj"
+  "C:/Users/zhi/OOP-Project/MS1/244_ms1_tester.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/244_ms1_tester.cpp.obj"
+  "C:/Users/zhi/OOP-Project/MS1/Date.cpp" "C:/Users/zhi/OOP-Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/MS1/Date.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Project.dir/MS1/01-DefValTester.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/MS1/02-ConstructorTester.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/MS1/03-LogicalOperator.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/MS1/04-ErrorHandling.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/MS1/244_ms1_tester.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/MS1/Date.cpp.obj"
+  "OOP_Project.pdb"
+  "OOP_Project.exe"
+  "OOP_Project.exe.manifest"
+  "libOOP_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
